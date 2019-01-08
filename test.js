256366edf8ba16502e64cbccaa8e694b0b006213
Titus Wormer <tituswormer@gmail.com>
2019-01-08 17:25:04 +0100

    Refactor tests

--- test.js
+++ test.js
@@ -10,52 +10,44 @@ var mixed = [
   'One sentence.  Two sentences.'
 ].join('\n')
 
-var many = 'One sentence.   Three sentences.'
-
-var nonSpace = 'One sentence.\tFour sentences.'
-
-test('retext-sentence-spacing(value[, size])', function(t) {
+test('sentenceSpacing(value[, size])', function(t) {
   var one = [null, 'space', 1]
 
   one.forEach(function(pref) {
-    t.equal(
-      String(
-        retext()
-          .use(spacing, {preferred: pref})
-          .processSync(mixed).messages
-      ),
-      '3:14-3:16: Expected `1` space between sentences, not `2`',
+    t.deepEqual(
+      retext()
+        .use(spacing, {preferred: pref})
+        .processSync(mixed)
+        .messages.map(String),
+      ['3:14-3:16: Expected `1` space between sentences, not `2`'],
       'should catch double spaces when preferred == ' + pref
     )
   })
 
-  t.equal(
-    String(
-      retext()
-        .use(spacing, {preferred: 2})
-        .processSync(mixed).messages
-    ),
-    '1:14-1:15: Expected `2` spaces between sentences, not `1`',
+  t.deepEqual(
+    retext()
+      .use(spacing, {preferred: 2})
+      .processSync(mixed)
+      .messages.map(String),
+    ['1:14-1:15: Expected `2` spaces between sentences, not `1`'],
     'should catch single spaces when preferred == 2'
   )
 
-  t.equal(
-    String(
-      retext()
-        .use(spacing)
-        .processSync(many).messages
-    ),
-    '1:14-1:17: Expected `1` space between sentences, not `3`',
+  t.deepEqual(
+    retext()
+      .use(spacing)
+      .processSync('One sentence.   Three sentences.')
+      .messages.map(String),
+    ['1:14-1:17: Expected `1` space between sentences, not `3`'],
     'should catch more than two spaces'
   )
 
-  t.equal(
-    String(
-      retext()
-        .use(spacing)
-        .processSync(nonSpace).messages
-    ),
-    '',
+  t.deepEqual(
+    retext()
+      .use(spacing)
+      .processSync('One sentence.\tFour sentences.')
+      .messages.map(String),
+    [],
     'should not emit messages for non-space white-space'
   )
 
